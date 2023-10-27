3438be74993687d671baa47f838b109f67ccbeab
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-10-27 23:26:02 +0300

    templates

--- app/base/views.py
+++ app/base/views.py
@@ -3,16 +3,31 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h2>home</h2>')
+    return render(request,'base/home.html')
 
 
 def posts(request):
-    return HttpResponse('<h2>posts</h2>')
+    return render(request,'base/posts.html')
 
 
 def post(request):
-    return HttpResponse('<h2>post</h2>')
+    return render(request,'base/post.html')
 
 
 def profile(request):
-    return HttpResponse('<h2>profile</h2>')
+    return render(request,'base/profile.html')
+
+# def home(request):
+#     return HttpResponse('<h2>home</h2>')
+#
+#
+# def posts(request):
+#     return HttpResponse('<h2>posts</h2>')
+#
+#
+# def post(request):
+#     return HttpResponse('<h2>post</h2>')
+#
+#
+# def profile(request):
+#     return HttpResponse('<h2>profile</h2>')
