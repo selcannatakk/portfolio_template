4d9c1561afd0a7e784e2d947f77b14c1df8cf4ac
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-11-03 19:06:22 +0300

    image field

--- app/base/models.py
+++ app/base/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
 class Post(models.Model):
     headline = models.CharField(max_length=200)
     sub_headline = models.CharField(max_length=200, null=True, blank=True)
-    # thumbnail
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
