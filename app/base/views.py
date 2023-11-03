ea500dbe6198e27dfb5d4a157dfff4c3019cb2ee
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-11-03 16:53:22 +0300

    refactor and template

--- app/base/views.py
+++ app/base/views.py
@@ -3,19 +3,34 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request,'base/index.html')
+    return render(request, 'base/index.html')
 
 
 def posts(request):
-    return render(request,'base/posts.html')
+    posts = [
+        {
+            'headline': 'Laboratory Management System',
+            'sub_headline': 'Designed built & mantained a the lab managment system for FOI Laboratories'
+        },
+        {
+            'headline': 'Online Store - CoursePost Title',
+            'sub_headline': 'Online store with paypal payments intergration and guest user shopping'
+        },
+        {
+            'headline': 'Membership Website',
+            'sub_headline': 'Modulized guide for online courses with step by step intructions<'
+        },
+    ]
+    context = {'posts': posts}
+    return render(request, 'base/posts.html', context)
 
 
 def post(request):
-    return render(request,'base/post.html')
+    return render(request, 'base/post.html')
 
 
 def profile(request):
-    return render(request,'base/profile.html')
+    return render(request, 'base/profile.html')
 
 # def home(request):
 #     return HttpResponse('<h2>home</h2>')
